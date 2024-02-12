61c364a15bb7daf7f86cfd42037aa01a5da41ed3
Maiten <cullen.maiten@gmail.com>
2024-02-12 11:11:03 -0300

    cambios index

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const express = require('express');
 const app = express();
-let cors = require('cors');
+const cors = require('cors');
 const bodyparser = require('body-parser');
-const https = require('https');
-let send = require('./controllers/correoController');
+const http = require('http'); // Requerir el módulo http
+const send = require('./controllers/correoController');
 const PORT = process.env.PORT || '80';
 
-
-//  app.use(cors());
-// const corsOptions = {
-//     origin: '*', // Permite cualquier origen
-//     methods: 'GET, POST, PUT, DELETE, OPTIONS', // Permite todos los métodos HTTP
-//     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept', // Cabeceras permitidas
-//   };
+const corsOptions = {
+    origin: '*', // Permite cualquier origen
+    methods: 'GET, POST, PUT, DELETE, OPTIONS', // Permite todos los métodos HTTP
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept', // Cabeceras permitidas
+  };
 
 app.use(bodyparser.json({ extended: true }));
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use(cors());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
-
-
+app.use(cors(corsOptions));
 function requestController(req, res){
     console.log('hola')
     res.send('hola nodejs')
@@ -37,22 +27,22 @@ app.post('/send', send.sendEmail);
         next();
  });
 
+ const server = https.createServer(requestController)
+ app.get("/", (req, res) => {
+     const htmlResponse = `
+     <html>
+         <head>
+             <title>Probando</title>
+         </head>
+         <body>
+             <h1>Hola</h1>
+         </body>
+     </html>
+     `;
+     res.send(htmlResponse);
+ })
+ 
 
-const server = https.createServer(requestController)
-app.get("/", (req, res) => {
-    const htmlResponse = `
-    <html>
-        <head>
-            <title>Probando</title>
-        </head>
-        <body>
-            <h1>Hola</h1>
-        </body>
-    </html>
-    `;
-    res.send(htmlResponse);
-})
-
-app.listen(PORT, ()=> {
+ app.listen(PORT, ()=> {
     console.log('escuchando' + PORT)
 })
